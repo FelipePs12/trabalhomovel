f2d8c927df154fe11a12cba11b86d8f7db887ded
FelipePs12 <felipe321ps@hotmail.com>
2023-09-11 21:19:13 -0300

    mensagem

--- trabalhomovel/App.js
+++ trabalhomovel/App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js todfsssdsddson your app!</Text>
+      <Text>Open up App.js todfsssfdfdsddson your app!</Text>
       <StatusBar style="auto" />
     </View>
   );
